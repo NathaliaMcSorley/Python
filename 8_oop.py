993e97dfbe9022330ac6dc41efd57915852eb6e9
Nathalia McSorley <nathaliamcsorley@gmail.com>
2022-12-28 14:12:50 -0600

    create oop file

---
+++ 8_oop.py
@@ -0,0 +1,18 @@
+print(f"\nPython Object Oriented Proegramming - MIMO App")
+print(f"****************************************************\n")
+
+print("-Encapsulating Objects")
+
+print(f"****************************************************\n")
+
+print("-Applying Inheritance in OOP")
+
+print(f"****************************************************\n")
+
+print("-Abstracting Objects")
+
+print(f"****************************************************\n")
+
+print("-Polymorphic Objects")
+
+print(f"****************************************************\n")
